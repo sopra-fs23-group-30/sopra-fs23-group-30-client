f81a98d46b9f19cfac86b9d9089639999a1d9412
Mika Schoch <98819916+ThePunisher77777@users.noreply.github.com>
2023-04-12 15:30:02 +0200

    Modified styling for MyListings page

--- src/ui/components/listing/ExpandableListingCard.js
+++ src/ui/components/listing/ExpandableListingCard.js
@@ -1,5 +1,6 @@
 import { Card } from "flowbite-react";
 import { useState } from "react";
+import Divider from "../general/Divider";
 
 function ExpandableListingCard(props) {
   const [isOpen, setIsOpen] = useState(false);
@@ -64,51 +65,54 @@ function ExpandableListingCard(props) {
       {isOpen && (
         <div className="flex flex-col gap-3">
           {props.listing?.applicants?.map((applicant) => (
-            <div className="flex flex-row w-full justify-between">
-              <div class="flex flex-row w-full gap-4">
-                <div>
-                  <img
-                    class="w-10 h-10 rounded-full"
-                    src="https://img.freepik.com/free-photo/portrait-african-american-man_23-2149072214.jpg"
-                    alt="Rounded avatar"
-                  />
+            <div className="flex flex-col">
+              <div className="flex flex-row w-full justify-between">
+                <div class="flex flex-row w-2/5 grid-cols-3 my-3 content-center">
+                  <div className="w-1/3">
+                    <img
+                      class="w-10 h-10 rounded-full"
+                      src="https://img.freepik.com/free-photo/portrait-african-american-man_23-2149072214.jpg"
+                      alt="Rounded avatar"
+                    />
+                  </div>
+                  <p className="text-sm my-3 w-1/3">{applicant.firstname}</p>
+                  <p className="text-sm my-3 w-1/3">{applicant.lastname}</p>
                 </div>
-                <p className="text-sm">{applicant.firstname}</p>
-                <p className="text-sm">{applicant.lastname}</p>
+
+                <div className="flex flex-row gap-3 my-3 content-center">
+                  <a
+                    href={"/profile/" + applicant.applicantId}
+                    class="h-6 text-center text-secondary bg-white focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-3 my-3 hover:underline focus:outline-none dark:focus:ring-blue-800"
+                  >
+                    See Profile
+                  </a>
+
+                  <button
+                    type="button"
+                    class="h-6 text-center text-white bg-primary hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-3 my-2 focus:outline-none dark:focus:ring-blue-800"
+                  >
+                    Actions
+                  </button>
+
+                  <svg
+                    data-accordion-icon="true"
+                    class="w-6 h-6 shrink-0 collapse"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                    onClick={() => {
+                      setIsOpen(true);
+                    }}
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                </div>
               </div>
-
-              <div className="flex flex-row gap-3">
-                <a
-                  href={"/profile/" + applicant.applicantId}
-                  class=" font-bold text-secondary bg-white focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-3 hover:underline focus:outline-none dark:focus:ring-blue-800"
-                >
-                  See Profile
-                </a>
-
-                <button
-                  type="button"
-                  class=" font-bold text-white bg-primary hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-3 focus:outline-none dark:focus:ring-blue-800"
-                >
-                  Actions
-                </button>
-
-                <svg
-                  data-accordion-icon="true"
-                  class="w-6 h-6 shrink-0 collapse"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                  onClick={() => {
-                    setIsOpen(true);
-                  }}
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
-              </div>
+              <hr className="w-full h-0.5 mx-auto bg-gray-100 border-0 rounded dark:bg-gray-700" />
             </div>
           ))}
         </div>
