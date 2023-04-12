30732d938b9720cf46896a737cbf6471c63341b9
fatihyag <fatih.yagan@uzh.ch>
2023-04-12 12:04:35 +0200

    created mylistings overview (+ see applicants) close #9

---
+++ src/ui/components/listing/ExpandableListingCard.js
@@ -0,0 +1,120 @@
+import { Card } from "flowbite-react";
+import { useState } from "react";
+
+function ExpandableListingCard(props) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <Card className="divide-y">
+      <div className="flex flex-row justify-between">
+        <p className="text-sm font-bold">{props.listing.listingTitle}</p>
+        <div className="flex flex-row gap-3">
+          <button
+            type="button"
+            class=" font-bold text-white bg-primary hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-3 py-1 focus:outline-none dark:focus:ring-blue-800"
+          >
+            Edit
+          </button>
+          <button
+            type="button"
+            class=" font-bold text-secondary bg-white focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-3 py-1 hover:underline focus:outline-none dark:focus:ring-blue-800"
+          >
+            Take Offline
+          </button>
+
+          {!isOpen && (
+            <svg
+              data-accordion-icon="true"
+              class="w-6 h-6 shrink-0"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+              onClick={() => {
+                setIsOpen(true);
+              }}
+            >
+              <path
+                fill-rule="evenodd"
+                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          )}
+
+          {isOpen && (
+            <svg
+              data-accordion-icon="true"
+              class="w-6 h-6 rotate-180 shrink-0"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+              onClick={() => {
+                setIsOpen(false);
+              }}
+            >
+              <path
+                fill-rule="evenodd"
+                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          )}
+        </div>
+      </div>
+      {isOpen && (
+        <div className="flex flex-col gap-3">
+          {props.listing?.applicants?.map((applicant) => (
+            <div className="flex flex-row w-full justify-between">
+              <div class="flex flex-row w-full gap-4">
+                <div>
+                  <img
+                    class="w-10 h-10 rounded-full"
+                    src="https://img.freepik.com/free-photo/portrait-african-american-man_23-2149072214.jpg"
+                    alt="Rounded avatar"
+                  />
+                </div>
+                <p className="text-sm">{applicant.firstname}</p>
+                <p className="text-sm">{applicant.lastname}</p>
+              </div>
+
+              <div className="flex flex-row gap-3">
+                <a
+                  href={"/profile/" + applicant.applicantId}
+                  class=" font-bold text-secondary bg-white focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-3 hover:underline focus:outline-none dark:focus:ring-blue-800"
+                >
+                  See Profile
+                </a>
+
+                <button
+                  type="button"
+                  class=" font-bold text-white bg-primary hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-3 focus:outline-none dark:focus:ring-blue-800"
+                >
+                  Actions
+                </button>
+
+                <svg
+                  data-accordion-icon="true"
+                  class="w-6 h-6 shrink-0 collapse"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                  onClick={() => {
+                    setIsOpen(true);
+                  }}
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+    </Card>
+  );
+}
+
+export default ExpandableListingCard;
