3d4b6c3293ae37259b21a60576bf71c58746728a
make2002 <ms.kern@gmx.ch>
2023-05-12 15:10:39 +0200

    Fixed merging errors

--- src/ui/views/Profile/Profile.js
+++ src/ui/views/Profile/Profile.js
@@ -10,7 +10,7 @@ import InputSelector from "ui/components/general/EditableInputSelector";
 import EditableLifespan from "ui/components/general/EditableLifespan";
 import EditableString from "ui/components/general/EditableString";
 import EditableTextarea from "ui/components/general/EditableTextarea";
-import {HiOutlineDownload } from 'react-icons/hi';
+import { HiOutlineDownload } from "react-icons/hi";
 
 export default function ProfilePage(props) {
   const [profileData, setProfileData] = useState(null);
@@ -379,110 +379,123 @@ export default function ProfilePage(props) {
                 onSave={handleGender}
                 canEdit={canEdit}
               />
-            )}
-            {canEdit ? (
-              <EditableTextarea
-                label="I am looking for"
-                content={profileData?.futureFlatmatesDescription}
-                onSave={handleFlatemateDescription}
-                canEdit={canEdit}
-            />
-            ) : (
-              <EditableTextarea
-              label={`${profileData?.firstname} is looking for`}
-              content={profileData?.futureFlatmatesDescription}
-              onSave={handleFlatemateDescription}
-              canEdit={canEdit}
-              />
-            )}
-            <label
-              htmlFor="small-input"
-              className="block text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Debt collection register extract
-            </label>
-            
-            {canEdit && (
-              <>
-                <div className="container text-center mt-1">
-                  <div {...getRootProps()}>
-                    <input {...getInputProps()} />
-                    <div class="flex items-center justify-center w-full">
-                      <label
-                        for="dropzone-file"
-                        class="flex flex-col items-center justify-center w-full h-32 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
-                      >
-                        {currentDocument ? (
-                          <div class="flex flex-col items-center justify-center pt-5 pb-6">
-                            {currentDocument.name}
-                          </div>
-                        ) : (
-                          <div class="flex flex-col items-center justify-center pt-5 pb-6">
-                            <svg
-                              aria-hidden="true"
-                              class="w-10 h-10 mb-3 text-gray-400"
-                              fill="none"
-                              stroke="currentColor"
-                              viewBox="0 0 24 24"
-                              xmlns="http://www.w3.org/2000/svg"
-                            >
-                              <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
-                              ></path>
-                            </svg>
-                            <p class="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                              <span class="font-semibold">Click to upload</span>{" "}
-                              or drag and drop
-                            </p>
-                            <p class="text-xs text-gray-500 dark:text-gray-400">
-                              PDF Document
-                            </p>
-                          </div>
-                        )}
-                      </label>
+              {canEdit ? (
+                <EditableTextarea
+                  label="I am looking for"
+                  content={profileData?.futureFlatmatesDescription}
+                  onSave={handleFlatemateDescription}
+                  canEdit={canEdit}
+                />
+              ) : (
+                <EditableTextarea
+                  label={`${profileData?.firstname} is looking for`}
+                  content={profileData?.futureFlatmatesDescription}
+                  onSave={handleFlatemateDescription}
+                  canEdit={canEdit}
+                />
+              )}
+              <label
+                htmlFor="small-input"
+                className="block text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Debt collection register extract
+              </label>
+              {canEdit && (
+                <>
+                  <div className="container text-center mt-1">
+                    <div {...getRootProps()}>
+                      <input {...getInputProps()} />
+                      <div class="flex items-center justify-center w-full">
+                        <label
+                          for="dropzone-file"
+                          class="flex flex-col items-center justify-center w-full h-32 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+                        >
+                          {currentDocument ? (
+                            <div class="flex flex-col items-center justify-center pt-5 pb-6">
+                              {currentDocument.name}
+                            </div>
+                          ) : (
+                            <div class="flex flex-col items-center justify-center pt-5 pb-6">
+                              <svg
+                                aria-hidden="true"
+                                class="w-10 h-10 mb-3 text-gray-400"
+                                fill="none"
+                                stroke="currentColor"
+                                viewBox="0 0 24 24"
+                                xmlns="http://www.w3.org/2000/svg"
+                              >
+                                <path
+                                  stroke-linecap="round"
+                                  stroke-linejoin="round"
+                                  stroke-width="2"
+                                  d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+                                ></path>
+                              </svg>
+                              <p class="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                                <span class="font-semibold">
+                                  Click to upload
+                                </span>{" "}
+                                or drag and drop
+                              </p>
+                              <p class="text-xs text-gray-500 dark:text-gray-400">
+                                PDF Document
+                              </p>
+                            </div>
+                          )}
+                        </label>
+                      </div>
                     </div>
                   </div>
-                </div>
-                </>)}
-            {profileData?.documentURL ? (
-              <a href={profileData.documentURL} class="text-sm text-blue-600 dark:text-blue-500 hover:underline">
-              <div class="flex items-center ">
-                <span>See debt collection register extract</span>
-                <HiOutlineDownload class="ml-2" size={20}/>
-              </div>
-            </a>
-            ) : (
-              <p class="text-sm text-gray-900 dark:text-gray-100">
-                No debt collection register extract uploaded
-              </p>
-            )}
-            {canEdit && (
-              <>
-                <div className="flex gap-2 mt-2">
-                  <Button className="bg-secondary hover:bg-primary" onClick={handlePDFUpload}>Upload</Button>
-                  <Button className="bg-red-600 hover:bg-red-700" onClick={handleRemoveFile}>
-                    Remove
-                  </Button>
-                </div>
-              </>
-            )}
-          </div>
-          <div className="w-full md:w-1/2 flex flex-col">
-            <EditableLifespan
-              label="Education"
-              canEdit={canEdit}
-              onChange={handleLifespansChanged}
-              lifespans={lifespans}
-            />
-            <EditableLifespan
-              label="Experience"
-              canEdit={canEdit}
-              onChange={handleLifespansChanged}
-              lifespans={lifespans}
-            />
+                </>
+              )}
+              {profileData?.documentURL ? (
+                <a
+                  href={profileData.documentURL}
+                  class="text-sm text-blue-600 dark:text-blue-500 hover:underline"
+                >
+                  <div class="flex items-center ">
+                    <span>See debt collection register extract</span>
+                    <HiOutlineDownload class="ml-2" size={20} />
+                  </div>
+                </a>
+              ) : (
+                <p class="text-sm text-gray-900 dark:text-gray-100">
+                  No debt collection register extract uploaded
+                </p>
+              )}
+              {canEdit && (
+                <>
+                  <div className="flex gap-2 mt-2">
+                    <Button
+                      className="bg-secondary hover:bg-primary"
+                      onClick={handlePDFUpload}
+                    >
+                      Upload
+                    </Button>
+                    <Button
+                      className="bg-red-600 hover:bg-red-700"
+                      onClick={handleRemoveFile}
+                    >
+                      Remove
+                    </Button>
+                  </div>
+                </>
+              )}
+            </div>
+            <div className="w-full md:w-1/2 flex flex-col">
+              <EditableLifespan
+                label="Education"
+                canEdit={canEdit}
+                onChange={handleLifespansChanged}
+                lifespans={lifespans}
+              />
+              <EditableLifespan
+                label="Experience"
+                canEdit={canEdit}
+                onChange={handleLifespansChanged}
+                lifespans={lifespans}
+              />
+            </div>
           </div>
         </div>
       </div>
