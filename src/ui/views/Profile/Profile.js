0007bae40b79de7a914e81f4efaf44b6d5a9204e
nyc6 <ddatsomor@swissonline.ch>
2023-05-14 21:16:38 +0200

    updated mobile view sections order

--- src/ui/views/Profile/Profile.js
+++ src/ui/views/Profile/Profile.js
@@ -400,12 +400,46 @@ export default function ProfilePage(props) {
                   canEdit={canEdit}
                 />
               )}
+              </div>
+              <div className="w-full md:w-1/2 flex flex-col mb-3">
+              <EditableLifespan
+                label="Education"
+                canEdit={canEdit}
+                onChange={handleLifespansChanged}
+                lifespans={lifespans}
+              />
+              <EditableLifespan
+                label="Experience"
+                canEdit={canEdit}
+                onChange={handleLifespansChanged}
+                lifespans={lifespans}
+              />
+            </div>
+            </div>
+            <div className="w-full md:w-1/2 flex flex-col">
               <label
                 htmlFor="small-input"
                 className="block text-sm font-medium text-gray-900 dark:text-white"
               >
                 Debt collection register extract
               </label>
+              {profileData?.documentURL ? (
+                <a
+                  href={profileData.documentURL}
+                  class="text-sm text-blue-600 dark:text-blue-500 hover:underline"
+                >
+                  <div class="flex items-center ">
+                    <span>See debt collection register extract</span>
+                    <svg fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" className="pl-1 w-5 h-5">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"></path>
+                    </svg>
+                  </div>
+                </a>
+              ) : (
+                <p class="text-sm text-gray-900 dark:text-gray-100">
+                  No debt collection register extract uploaded
+                </p>
+              )}
               {canEdit && (
                 <>
                   <div className="container text-center mt-1">
@@ -452,27 +486,6 @@ export default function ProfilePage(props) {
                       </div>
                     </div>
                   </div>
-                </>
-              )}
-              {profileData?.documentURL ? (
-                <a
-                  href={profileData.documentURL}
-                  class="text-sm text-blue-600 dark:text-blue-500 hover:underline"
-                >
-                  <div class="flex items-center ">
-                    <span>See debt collection register extract</span>
-                    <svg fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" className="pl-1 w-5 h-5">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"></path>
-                    </svg>
-                  </div>
-                </a>
-              ) : (
-                <p class="text-sm text-gray-900 dark:text-gray-100">
-                  No debt collection register extract uploaded
-                </p>
-              )}
-              {canEdit && (
-                <>
                   <div className="flex gap-2 mt-2">
                     <Button
                       className="bg-secondary hover:bg-primary"
@@ -489,24 +502,9 @@ export default function ProfilePage(props) {
                   </div>
                 </>
               )}
-            </div>
-            <div className="w-full md:w-1/2 flex flex-col">
-              <EditableLifespan
-                label="Education"
-                canEdit={canEdit}
-                onChange={handleLifespansChanged}
-                lifespans={lifespans}
-              />
-              <EditableLifespan
-                label="Experience"
-                canEdit={canEdit}
-                onChange={handleLifespansChanged}
-                lifespans={lifespans}
-              />
-            </div>
+              </div>
           </div>
         </div>
       </div>
-    </div>
   );
 }
